1dfcb15146e72af549da15082a91bdc68bc66b9c
Cristina Matsunaga <criismnaga@gmail.com>
2023-11-22 09:22:46 -0300

    Tests atualization according to classroom

--- src/br/gov/cesarschool/poo/bonusvendas/testes/TesteEntidades.java
+++ src/br/gov/cesarschool/poo/bonusvendas/testes/TesteEntidades.java
@@ -1,36 +1,49 @@
 package br.gov.cesarschool.poo.bonusvendas.testes;
 
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
-class ComparadoraObjetosSerial {
-	static boolean compareObjectsSerial(Serializable s1, Serializable s2) {
-		ByteArrayOutputStream  bos1 = null;
-		ByteArrayOutputStream  bos2 = null;
-		ObjectOutputStream oos1 = null;
-		ObjectOutputStream oos2 = null;
-		boolean ret = true;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import br.gov.cesarschool.poo.bonusvendas.entidade.CaixaDeBonus;
+import br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
+import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Endereco;
+import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Sexo;
+
+public class TesteEntidades {
+	@Test
+	public void testCalcularIdade() {
+		LocalDate dataNasc = LocalDate.parse("2001-01-01");
+		Vendedor vend = new Vendedor("12345678901", "Carlos Calmon", Sexo.MASCULINO, 
+		        dataNasc,
+		        2500.0,
+		        new Endereco("RUA A", 101, "BL B", "51001001", "RECIFE", "PE", "BRASIL"));
+		int idadeEsp = (int)ChronoUnit.YEARS.between(dataNasc, LocalDate.now());
+		int idade = vend.calcularIdade();
+		Assertions.assertEquals(idadeEsp, idade);		
+	}
+	@Test
+	public void testCreditarDebitar() {
+		CaixaDeBonus cb = new CaixaDeBonus(12345678920231109L);
+		LocalDateTime dtRef = LocalDateTime.parse("2001-01-01T00:00:00");
 		try {
-			bos1 = new ByteArrayOutputStream();
-			bos2 = new ByteArrayOutputStream();
-			oos1 = new ObjectOutputStream(bos1);
-			oos2 = new ObjectOutputStream(bos2);
-			oos1.writeObject(s1);
-			oos2.writeObject(s2);
-			byte[] b1 = bos1.toByteArray();
-			byte[] b2 = bos2.toByteArray();			
-			for (int i=0; i<b1.length; i++) {
-				if (b2[i] != b1[i]) {
-					ret = false;
-					break;
-				}
-				i++;
-			}
+			Class<?> c = CaixaDeBonus.class;		
+			Field field = c.getDeclaredField("dataHoraAtualizacao");
+			field.setAccessible(true);			
+			field.set(cb, dtRef);
 		} catch (Exception e) {
+			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
-		return ret; 
+		cb.creditar(100.0);
+		Assertions.assertEquals(100.0, cb.getSaldo());
+		cb.creditar(200.0);
+		Assertions.assertEquals(300.0, cb.getSaldo());
+		cb.debitar(120.0);
+		Assertions.assertEquals(180.0, cb.getSaldo());
+		Assertions.assertTrue(dtRef.isBefore(cb.getDataHoraAtualizacao()));
 	}
-
 }
