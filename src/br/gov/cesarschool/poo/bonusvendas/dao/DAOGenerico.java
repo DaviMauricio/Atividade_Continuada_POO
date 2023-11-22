4779afb587bd8ab2bb24db3cefeec340a95aa65b
Cristina Matsunaga <criismnaga@gmail.com>
2023-11-22 09:39:49 -0300

    class DAOgenerico added

--- src/br/gov/cesarschool/poo/bonusvendas/dao/DAOGenerico.java
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/DAOGenerico.java
@@ -1,31 +1,45 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
-
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Registro;
+import java.io.Serializable;
 
-public class DAOGenerico
-{
-	CadastroObjetos cadastro;
-	
-	public DAOGenerico(CadastroObjetos cadastro) {
-		// revisar
-		this.cadastro = cadastro;
-	}
-	
-	// falta complementar
-	boolean incluir(Registro reg) {
-		return true;
-	}
-	
-	boolean alterar(Registro reg) {
-		return true;
-	}
-	
-	Registro buscar(String id) {
-		return;
-	}
-	
-	Registro[] buscarTodos() {
-		return;
-	}
+public class DAOGenerico {
+    private CadastroObjetos cadastro;
+
+    public DAOGenerico(Class<?> tipo) {
+        this.cadastro = new CadastroObjetos(tipo);
+    }
+
+    public boolean incluir(Registro reg) {
+        Registro regBusca = buscar(reg.getIdUnico());
+        if (regBusca != null) {
+            return false;
+        } else {
+            cadastro.incluir(reg, reg.getIdUnico());
+            return true;
+        }         
+    }
+    
+    public boolean alterar(Registro reg) {
+        Registro regBusca = buscar(reg.getIdUnico());
+        if (regBusca == null) {
+            return false;
+        } else {
+            cadastro.alterar(reg, reg.getIdUnico());
+            return true;
+        }       
+    }
+    
+    public Registro buscar(String id) {
+        return (Registro)cadastro.buscar(id);
+    }
+    
+    public Registro[] buscarTodos() {
+        Serializable[] rets = cadastro.buscarTodos(Registro.class);
+        Registro[] registros = new Registro[rets.length];
+        for(int i = 0; i < rets.length; i++) {
+            registros[i] = (Registro)rets[i];
+        }
+        return registros;
+    } 
 }
